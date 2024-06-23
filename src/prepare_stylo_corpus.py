d1ea8db8377e88b2707824f56ed89ab573831cd5
Pablo Ruiz Fabo <ruizfabo@unistra.fr>
2024-06-23 13:12:20 +0200

    [mn] [dt] stylo formatting script iterates over model response dirs

--- src/prepare_stylo_corpus.py
+++ src/prepare_stylo_corpus.py
@@ -14,10 +14,12 @@ if __name__ == '__main__':
   reload(ut)
   if not os.path.exists(cf.stylo_corpus_dir):
     os.makedirs(cf.stylo_corpus_dir)
-  mod_dir = os.path.join(cf.stylo_corpus_dir,
-                         ut.clean_model_name(models_to_use[0]))
-  if not os.path.exists(mod_dir):
-    os.makedirs(mod_dir)
-  techno_resp_dir = os.path.join(cf.response_dir, "gpt")
-  resp_dir = os.path.join(techno_resp_dir, ut.clean_model_name(models_to_use[0]))
-  ut.message_to_stylo_for_dir(resp_dir, mod_dir, cf.metadata_file)
+  for mod_name in models_to_use:
+    print("- Model:", mod_name)
+    mod_dir = os.path.join(cf.stylo_corpus_dir,
+                           ut.clean_model_name(mod_name))
+    if not os.path.exists(mod_dir):
+      os.makedirs(mod_dir)
+    techno_resp_dir = os.path.join(cf.response_dir, "gpt")
+    resp_dir = os.path.join(techno_resp_dir, ut.clean_model_name(mod_name))
+    ut.message_to_stylo_for_dir(resp_dir, mod_dir, cf.metadata_file)

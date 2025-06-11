834fd318a7a47ebb4e79a90b01940e629ea14ac1
Pablo Ruiz Fabo <pablo.ruiz.fabo@usc.gal>
2025-06-11 22:54:01 +0200

    more batches

--- src/config.py
+++ src/config.py
@@ -30,17 +30,19 @@ model_list_for_postpro = ["gpt-4o-mini"]
 
 
 # Open AI ---------------------------------------------------------------------
-oai_models = ["gpt-3.5-turbo", "gpt-4o"]  # , "gpt-4", "gpt-4-turbo", "gpt-4o"]
+#oai_models = ["gpt-3.5-turbo", "gpt-4o"]  # , "gpt-4", "gpt-4-turbo", "gpt-4o"]
+oai_models = ["gpt-4.1"]  # , "gpt-4", "gpt-4-turbo", "gpt-4o"]
 oai_config = {
   "temperature": 1,
   "top_p": 1,
-  #"seed": 14
+  "seed": 14,
   "number_of_completions_humor" : 5,
   "number_of_completions_general" : 1
 }
 
 # Mistral ---------------------------------------------------------------------
-mistral_models = ["mistral-large-latest"]#, "mistral-small", "mistral-large-latest", "gpt-3.5-turbo", "gpt-4o"]
+mistral_models = ["mistral-medium"]#, "mistral-small", "mistral-large-latest", "gpt-3.5-turbo", "gpt-4o"]
+#mistral_models = ["mistral-large-latest"]#, "mistral-small", "mistral-large-latest", "gpt-3.5-turbo", "gpt-4o"]
 mistral_config = {
   "temperature": 0.7,
   "top_p": 1,
@@ -51,17 +53,20 @@ mistral_config = {
 }
 
 # Anthropic -------------------------------------------------------------------
-anthropic_models_actual_versions = {'claude-sonnet-latest': 'claude-sonnet-4-20250514', 'claude-3-5-haiku-latest': 'claude-3-5-haiku-20241022'}
+anthropic_models_actual_versions = {'claude-3-5-sonnet-latest': 'claude-3-5-sonnet-20241022', 'claude-3-5-haiku-latest': 'claude-3-5-haiku-20241022'}
 #anthropic_models = ['claude-sonnet-latest', 'claude-3-5-haiku-latest']
+anthropic_models = ['claude-sonnet-4-20250514']
 #anthropic_models = ['claude-3-5-haiku-latest']
-anthropic_models = ['claude-3-5-sonnet-latest']
+#anthropic_models = ['claude-3-5-sonnet-latest']
 anthropic_config = {
   "temperature": 1,
   "max_tokens": 500,
 }
 
 # Deepseek ---------------------------------------------------------------------
-deepseek_models = ["deepseek-chat"]
+# chat is https://api-docs.deepseek.com/news/news250325 and reasoner is https://api-docs.deepseek.com/news/news250528
+# DeepSeek-V3-0324 and DeepSeek-R1-0528 
+deepseek_models = ["deepseek-reasoner"]
 #deepseek_models = ["deepseek-chat", "deepseek-reasoner"]
 deepseek_config = {
   "temperature": 1,
@@ -70,6 +75,18 @@ deepseek_config = {
   "number_of_completions_humor" : 1,
   "number_of_completions_general" : 1
 }
+
+# Gemini ---------------------------------------------------------------------
+#gemini_models = ["gemini-2.0-flash"]
+gemini_models = ["gemini-1.5-pro"]
+gemini_config = {
+  "temperature": 0.9,
+  #"top_p": 1,
+  #"seed": 14
+  "number_of_completions_humor" : 5,
+  "number_of_completions_general" : 1
+}
+
 
 # Analysis --------------------------------------------------------------------
 
